807133d20a64597f6f4df6ac7fd62083915a116a
Yukhnenko Nataliia <101944838+Natali2721@users.noreply.github.com>
2023-01-14 15:02:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,39 @@
-const argv = require("yargs").argv;
+const contacts = require("./contacts");
 
-// TODO: рефакторить
-function invokeAction({ action, id, name, email, phone }) {
+const yargs = require("yargs");
+const { hideBin } = require("yargs/helpers");
+
+async function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
     case "list":
-      // ...
+      const allContacts = await contacts.listContacts();
+      console.table(allContacts);
       break;
 
     case "get":
-      // ... id
+      const contact = await contacts.getContactById(id);
+      if (!contact) {
+        throw new Error(`Contact with id=${id} not found`);
+      }
+      console.log(contact);
       break;
 
     case "add":
-      // ... name email phone
+      const newContact = await contacts.addContact(name, email, phone);
+      console.log(newContact);
       break;
 
     case "remove":
-      // ... id
+      const removeContact = await contacts.removeContact(id);
+      console.log(removeContact);
       break;
 
     default:
       console.warn("\x1B[31m Unknown action type!");
   }
 }
+
+const arr = hideBin(process.argv);
+const { argv } = yargs(arr);
 
 invokeAction(argv);
